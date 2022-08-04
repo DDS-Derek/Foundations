7da0d3eed4e611d2d72f37f85d216dff9a4fac89
DDSRem <1448139087@qq.com>
2022-08-04 12:07:35 +0800

    update

--- Ubuntu18.04-nginx1.16.1-php7.2.21/shell/000-start.sh
+++ Ubuntu18.04-nginx1.16.1-php7.2.21/shell/000-start.sh
@@ -6,6 +6,10 @@ if [ ! -f "/tz.lock" ]; then
  bash /shell/010-tz.sh
 fi
 
+# 设置PUID PGID
+echo "\033[34m设置PUID PGID... \033[0m"
+sh /shell/011-adduser.sh
+
 /usr/local/php/sbin/php-fpm
 
 /usr/local/nginx/sbin/nginx
