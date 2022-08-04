5695950503cb0e3fcb11ce5ce84c7af6a7582bf9
DDSRem <1448139087@qq.com>
2022-08-04 11:59:49 +0800

    update

--- Debain-nginx1.18.0-php7.4-sql/shell/000-start.sh
+++ Debain-nginx1.18.0-php7.4-sql/shell/000-start.sh
@@ -2,14 +2,14 @@
 
 # 设置时区
 if [ ! -f "/tz.lock" ]; then
- echo -e "\033[34m设置时区... \033[0m"
+ echo "\033[34m设置时区... \033[0m"
  sh /shell/010-tz.sh
 fi
 
 # 设置PUID PGID
-echo -e "\033[34m设置PUID PGID... \033[0m"
+echo "\033[34m设置PUID PGID... \033[0m"
 sh /shell/011-adduser.sh
 
 # 启动nginx
-echo -e "\033[34mNginx启动 \033[0m"
-nginx -g "daemon off;"
+echo "\033[34m启动 \033[0m"
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
